955a0aa4af3596a31192abb788aa497a09f45913
Bianca <bmstorm13@users.noreply.github.com>
2020-02-29 15:03:41 -0800

    python tkinter

--- Python_Drills/check_files_tkinter/checkfiles_func.py
+++ Python_Drills/check_files_tkinter/checkfiles_func.py
@@ -11,28 +11,17 @@
 # Tested OS:  This code was written and tested to work with Windows 10.
 
 import os
+from tkinter import filedialog
 from tkinter import *
 import tkinter as tk
-import sqlite3
+
 
 
 # Be sure to import our other modules 
 # so we can have access to them
-import checkfiles_main
+
 import checkfiles_gui
 
-"""
-
-def center_window(self, w, h): # pass in the tkinter frame (master) reference and the w and h
-    # get user's screen width and height
-    screen_width = self.master.winfo_screenwidth()
-    screen_height = self.master.winfo_screenheight()
-    # calculate x and y coordinates to paint the app centered on the user's screen
-    x = int((screen_width/2) - (w/2))
-    y = int((screen_height/2) - (h/2))
-    centerGeo = self.master.geometry('{}x{}+{}+{}'.format(w, h, x, y))
-    return centerGeo
-"""
 
 # catch if the user's clicks on the windows upper-right 'X' to ensure they want to close
 def ask_quit(self):
@@ -40,6 +29,25 @@ def ask_quit(self):
         # This closes app
         self.master.destroy()
         os._exit(0)
+
+        
+
+#askdirectory on browse button
+def browse_button(self):
+    foldername = filedialog.askdirectory()
+    self.folder_path.set(foldername)
+
+
+    print(foldername)
+    return foldername
+
+"""
+root = Tk()
+v = StringVar()
+button2 = Button(text="Browse", command=browse_button).grid(row=0, column=3)
+
+mainloop()
+"""
 
 if __name__ == "__main__":
     root = tk.Tk()
--- Python_Drills/check_files_tkinter/checkfiles_gui.py
+++ Python_Drills/check_files_tkinter/checkfiles_gui.py
@@ -3,7 +3,7 @@
 #
 # Python Ver:   3.5.1
 #
-# Author:       Daniel A. Christie
+# Author:       Daniel A. Christi#e
 #
 # Purpose:      Phonebook Demo. Demonstrating OOP, Tkinter GUI module,
 #               using Tkinter Parent and Child relationships.
@@ -11,27 +11,46 @@
 # Tested OS:  This code was written and tested to work with Windows 10.
 
 
+import os
 from tkinter import *
 import tkinter as tk
 
-# Be sure to import our other modules 
-# so we can have access to them
-import checkfiles_main
-#import checkfiles_func
+#import function file so we can have access to them
+import checkfiles_func
 
+# Frame is the Tkinter frame class that our own class will inherit from
+class ParentWindow(Frame):
+    def __init__(self, master, *args, **kwargs):
+        Frame.__init__(self, master, *args, **kwargs)
+
+        # define our master frame configuration
+        self.master = master
+        self.master.minsize(500,200) #(Height, Width)
+        self.master.maxsize(500,200)  #with max size same as minsize, the user cannot change size of window
+        self.master.title("Check Files")
+        self.master.configure(bg="#F0F0F0")
+        self.folder_path = StringVar()
+        # This protocol method is a tkinter built-in method to catch if 
+        # the user clicks the upper corner, "X" on Windows OS.
+        #self.master.protocol("WM_DELETE_WINDOW",lambda: checkfiles_func.ask_quit(self))
+        #arg = self.master
+
+        # load in the GUI widgets from a separate module, 
+        # keeping your code comparmentalized and clutter free
+        load_gui(self)
 
 
 def load_gui(self):
     #settin up the grid - left buttons
-    self.btn_browse = tk.Button(self.master,width=15,height=1,text='Browse...')#,command=lambda: checkfiles_func.browse(self))
+    self.btn_browse = tk.Button(self.master,width=15,height=1,text='Browse...',command=lambda: checkfiles_func.browse_button(self))
     self.btn_browse.grid(row=1,column=1,rowspan=1,padx=(20,0),pady=(20,0),sticky=E+N)
-    self.btn_browse = tk.Button(self.master,width=15,height=1,text='Browse...')#,command=lambda: checkfiles_func.browse(self))
+    self.btn_browse = tk.Button(self.master,width=15,height=1,text='Browse...')#,command=lambda: browse_button(self))
     self.btn_browse.grid(row=2,column=1,rowspan=1,padx=(20,0),pady=(20,0),sticky=E+N)
-    self.btn_check = tk.Button(self.master,width=15,height=2,text='Check for files...')#,command=lambda: checkfiles_func.check(self))
+    self.btn_check = tk.Button(self.master,width=15,height=2,text='Check for files...')#,command=lambda: checkfiles_func.check_button(self))
     self.btn_check.grid(row=3,column=1,rowspan=2,padx=(20,0),pady=(20,0),sticky=E+N)
 
     #right blank boxes
-    self.txt_browse = tk.Entry(self.master,width=50,text='')
+    self.txt_browse = tk.Entry(self.master,width=50,textvariable=self.folder_path)
     self.txt_browse.grid(row=1,column=2,rowspan=1,columnspan=15,padx=(20,0),pady=(20,0),sticky=S+W)
     self.txt_browse = tk.Entry(self.master,width=50,text='')
     self.txt_browse.grid(row=2,column=2,rowspan=1,columnspan=15,padx=(20,0),pady=(20,0),sticky=S+W)
@@ -42,5 +61,8 @@ def load_gui(self):
 
 
 
+
 if __name__ == "__main__":
-    pass
+    root = tk.Tk()
+    App = ParentWindow(root)
+    root.mainloop()
